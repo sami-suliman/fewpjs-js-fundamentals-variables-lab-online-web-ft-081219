f122cc87a1a18ce8044839faeb5056862211dd82
sami suliman <sami.bco@gmail.com>
2019-11-28 11:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solution in this file!
 
 const companyName ='Scuber';
+<<<<<<< HEAD
 let  mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
+=======
+>>>>>>> 82083dccdcf38a69ba7d12b299e8684f89591343
